28be1e3d352a3646a3d5aa3bcbfbc51341c654b1
Erin Woody <erinelizabethwoody@gmail.com>
2017-08-14 16:47:32 -0500

    applied moment and chalk packages

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var moment = require('moment');
-moment().format();
-
-
-
 const chalk = require('chalk');
 
-console.log(chalk.blue('Hello world!'));
+let now = new moment();
+console.log("It is " + chalk.blue.bold(moment().format("dddd, MMMM Do YYYY, h:mm:ss a")) + ".");
+
+console.log("It is the " + chalk.magenta.bold(now.dayOfYear() + "th") + " day of the year.");
+
+console.log("It is " + chalk.green.bold(now.second() + (now.minute() * 60) + (now.hour() * 360)), "seconds into the day.");
+
+if (now.isDST() === true) {
+    console.log("It" + chalk.hex('#5A6C40').bold(" is ") + ("Daylight Savings Time."))
+}
+
+if (now.isLeapYear() === false) {
+    console.log("It" + chalk.hex('#AB4245').bold(" is not ") + ("a Leap Year."))
+}
